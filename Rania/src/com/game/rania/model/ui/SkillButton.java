8a9e83a4476211d7f5b42dea1c4e79542b975af4
Tim <dudkinta@yandex.ru>
2013-07-29 18:37:45 +0400

    *prepare check use equips

--- Rania/src/com/game/rania/model/ui/SkillButton.java
+++ Rania/src/com/game/rania/model/ui/SkillButton.java
@@ -4,7 +4,10 @@ import com.game.rania.RaniaGame;
 import com.game.rania.controller.Controllers;
 import com.game.rania.model.Player;
 import com.game.rania.model.Target;
+import com.game.rania.model.items.Device;
 import com.game.rania.model.items.Equip;
+import com.game.rania.model.items.Item;
+import com.game.rania.model.element.Object;
 
 public class SkillButton extends PressedButton{
 	
@@ -18,11 +21,42 @@ public class SkillButton extends PressedButton{
 			@Override
 			public void execute(boolean touch) {
 				Player player = Controllers.locController.getPlayer();
-				if (player == null || player.target.type != Target.user)
+				if (player.body.wear<=0||skillEquip.wear<=0)
 					return;
-
-				skillEquip.item.use(player, player.target.object);
-				Controllers.netController.sendUseEquip(skillEquip.id);
+				int itemType =  skillEquip.item.itemType;
+				switch (itemType) {
+					case Item.Type.device: {
+						Device dev = (Device)skillEquip.item;
+						int deviceType = dev.deviceType;
+						switch (deviceType) {
+							case Device.Type.weapon: {
+								if ((player == null || player.target.type != Target.user || player.target.user == player.id))
+									return;
+								
+								skillEquip.item.use(player, player.target.object);
+								Controllers.netController.sendUseEquip(skillEquip.id);
+								break;
+							}
+							case Device.Type.droid: {
+								Object targetObj = player.target.object;
+								if (player.target.type!= Target.user) 
+									targetObj = player;
+								skillEquip.item.use(player, targetObj);
+								break;
+							}
+							case Device.Type.hyper: {
+								break;
+							}
+							case Device.Type.shield: {
+								break;
+							}
+						}
+						break;
+					}
+					case Item.Type.consumable: {
+						break;
+					}
+				}
 			}
 		};
 	}

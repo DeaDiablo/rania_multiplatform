794913afaa0e74ac2f6d0d4e27d43535ca3ea186
DeaDiablo <deadiablo@gmail.com>
2013-08-06 22:58:21 +0400

    * fixed Indexes for infoText

--- Rania/src/com/game/rania/model/Indexes.java
+++ Rania/src/com/game/rania/model/Indexes.java
@@ -2,7 +2,6 @@ package com.game.rania.model;
 
 public class Indexes
 {
-
   static public final int nebulas    = 3;
   static public final int object     = 10;
   static public final int button     = 11;
@@ -10,7 +9,7 @@ public class Indexes
   static public final int star       = 14;
   static public final int planets    = 15;
   static public final int users      = 20;
-  static public final int infoText   = 30;
   static public final int player     = 100;
   static public final int ammunition = 110;
+  static public final int infoText   = 120;
 }
